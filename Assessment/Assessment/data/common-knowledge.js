1ebb95e17e426c91887d9236545f87098a9be144
Aditya Hada <99535849+Aditya00101@users.noreply.github.com>
2024-02-09 15:58:10 +0530

    Add files via upload

---
+++ Assessment/Assessment/data/common-knowledge.js
@@ -0,0 +1,209 @@
+const quizData = {
+  title: "Quize app Application"
+};
+
+const questions = [
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "Aditya", correct: true },
+      { text: "ritik", correct: false },
+      { text: "akshay", correct: false},
+      { text: "sachin", correct: false }
+    ]
+  },
+  {
+    text: "What is my favourite place ?",
+    type: "mc",
+    answers: [
+      { text: "Delhi", correct: false },
+      { text: "Maligram,pingla", correct: true },
+      { text: "Kolkata", correct: false },
+      { text: "Mumbai", correct: false }
+    ]
+  },
+  {
+    text: "What is my favourite place ?",
+    type: "mc",
+    answers: [
+      { text: "Delhi", correct: false },
+      { text: "Maligram,pingla", correct: true },
+      { text: "Kolkata", correct: false },
+      { text: "Mumbai", correct: false }
+    ]
+  }
+
+];
+
+module.exports = { quizData, questions };
